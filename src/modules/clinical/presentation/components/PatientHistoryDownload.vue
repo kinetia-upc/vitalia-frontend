67935fa1c58eb0d84860336a99bac594a8143745
AleeAsto <astocondor.alejandra@gmail.com>
2026-05-13 01:19:35 -0500

    feat: add patient history view

---
+++ src/modules/clinical/presentation/components/PatientHistoryDownload.vue
@@ -0,0 +1,30 @@
+<script setup>
+defineProps({
+  disabled: {
+    type: Boolean,
+    default: false
+  },
+  labels: {
+    type: Object,
+    required: true
+  }
+})
+
+const emit = defineEmits(['download'])
+</script>
+
+<template>
+  <button
+    class="history-download-card"
+    type="button"
+    :disabled="disabled"
+    @click="emit('download')"
+  >
+    <span>PDF</span>
+    <div>
+      <strong>{{ labels.downloadDossier }}</strong>
+      <small>{{ labels.pdfExport }}</small>
+    </div>
+    <i>›</i>
+  </button>
+</template>
