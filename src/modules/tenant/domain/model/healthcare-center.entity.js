3aa06f2104e4b8ffb2e2b344e17c1176949f0923
Leotens <leotens11@gmail.com>
2026-07-08 15:50:07 -0500

    feat: support healthcare center image url

--- src/modules/tenant/domain/model/healthcare-center.entity.js
+++ src/modules/tenant/domain/model/healthcare-center.entity.js
@@ -2,16 +2,26 @@ export class HealthcareCenter {
     constructor({
         id = null,
         code = "",
+        Code = "",
         healthcareCenterName = "",
+        HealthcareCenterName = "",
         allianceStartDate = null,
+        AllianceStartDate = null,
         allianceFinishDate = null,
-        rucNumber = null
+        AllianceFinishDate = null,
+        rucNumber = null,
+        RucNumber = null,
+        imageUrl = "",
+        imageURL = "",
+        ImageUrl = "",
+        ImageURL = ""
     } = {}) {
         this.id = id;
-        this.code = code;
-        this.healthcareCenterName = healthcareCenterName;
-        this.allianceStartDate = allianceStartDate;
-        this.allianceFinishDate = allianceFinishDate;
-        this.rucNumber = rucNumber;
+        this.code = code || Code;
+        this.healthcareCenterName = healthcareCenterName || HealthcareCenterName;
+        this.allianceStartDate = allianceStartDate ?? AllianceStartDate;
+        this.allianceFinishDate = allianceFinishDate ?? AllianceFinishDate;
+        this.rucNumber = rucNumber ?? RucNumber;
+        this.imageUrl = imageUrl || imageURL || ImageUrl || ImageURL;
     }
 }
